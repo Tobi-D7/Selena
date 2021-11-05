a99cbd1ae40b0322caeba78083c259cb29ad4688
Tobi <tobid7@outlook.de>
2021-11-05 15:02:22 +0100

    N

--- source/codegen_neo.cpp
+++ source/codegen_neo.cpp
@@ -401,8 +401,7 @@ neocode_program CGNeoBuildProgramInstance(ast_node *ASTNode, symtable *S) {
   cg_neo CGNeo;
   CGNeo.SymbolTable = S;
   Program.Registers = {};
-  Program.Globals.push_back(
-      (neocode_variable){"NS_Position",
+  Program.Globals.push_back((neocode_variable){"NS_Position",
                          "vec4",
                          ast_node::STRUCT,
                          Program.Registers.AllocOutput(),
@@ -451,13 +450,13 @@ neocode_program CGNeoBuildProgramInstance(ast_node *ASTNode, symtable *S) {
                                                neocode_variable::OUTPUT_VIEW,
                                                {0},
                                                0});
-  /*Program.Globals.push_back((neocode_variable){"NS_Normalquat",
+  Program.Globals.push_back((neocode_variable){"NS_Normalquat",
                                                "vec4",
                                                ast_node::STRUCT,
                                                Program.Registers.AllocOutput(),
                                                neocode_variable::OUTPUT_QUATERNION,
                                                {0},
-                                               0});*/
+                                               0});
   Program.Registers.AllocConstant();
   Program.Registers.AllocConstant();
   Program.Registers.AllocConstant();
