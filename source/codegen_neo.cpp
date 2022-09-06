c3933db3e3fe9663286dc5a893c0e1022d31456d
Tobi <tobid7@outlook.de>
2022-09-06 22:04:01 +0200

    Fix nquad maybe

--- source/codegen_neo.cpp
+++ source/codegen_neo.cpp
@@ -408,6 +408,13 @@ neocode_program CGNeoBuildProgramInstance(ast_node *ASTNode, symtable *S) {
                          neocode_variable::OUTPUT_POSITION,
                          {0},
                          0});
+  Program.Globals.push_back((neocode_variable){"NS_Normal",
+                                               "vec4",
+                                               ast_node::STRUCT,
+                                               Program.Registers.AllocOutput(),
+                                               neocode_variable::OUTPUT_QUATERNION,
+                                               {0},
+                                               0});
   Program.Globals.push_back((neocode_variable){"NS_Color",
                                                "vec4",
                                                ast_node::STRUCT,
@@ -450,13 +457,6 @@ neocode_program CGNeoBuildProgramInstance(ast_node *ASTNode, symtable *S) {
                                                neocode_variable::OUTPUT_VIEW,
                                                {0},
                                                0});
- /* Program.Globals.push_back((neocode_variable){"NS_Normal",
-                                               "vec4",
-                                               ast_node::STRUCT,
-                                               Program.Registers.AllocOutput(),
-                                               neocode_variable::OUTPUT_QUATERNION,
-                                               {0},
-                                               0});*/
   Program.Registers.AllocConstant();
   Program.Registers.AllocConstant();
   Program.Registers.AllocConstant();
